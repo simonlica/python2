4350fad4cd9273c11e49f5f263dcc53d914e8065
simonlica <carvalho.heloisa@escola.pr.gov.br>
2024-04-01 12:00:06 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+print("Programa Expreeso\m")
+print("1 Cadastrar Restaurante")
+print("2 Listar Restaurante")
+print("3 Ativar Restaurante")
+print("4 Sair\n")
+
+input("Escolha uma opção")
+
+opcao_digitada = input
+
+print("Você escolheu:", opcao_digitada)
